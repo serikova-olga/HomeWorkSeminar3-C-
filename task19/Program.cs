9a1458ce48525f08aad1221fd3f1eb94dd252b70
OLGA <serikova-olga@mail.ru>
2022-06-28 14:29:36 +0300

    task19rev

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,3 +3,26 @@
 23432 -> да
 12821 -> да*/
 
+Console.Write("input five digit number: ");
+int n = Convert.ToInt32(Console.ReadLine()) ;
+
+if (n > 99999 || n < 1000)
+{
+    Console.WriteLine("input five digit number: ");
+    return;
+}
+
+int digit1 = n / 10000;
+int digit2 = n / 1000 %10;
+int digit5 = n % 10;
+int digit4 = n /10 % 10;
+
+if (digit1==digit5 && digit2==digit4)
+{
+    Console.WriteLine ("yes");
+}
+
+else
+{
+    Console.WriteLine ("No");
+}
